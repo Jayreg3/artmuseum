1efc2cf120cd295aa2ec011a39a23230d66c9819
Jayreg3 <jeregistre@gmail.com>
2018-09-17 02:58:21 -0400

    Show objects functions as a function

--- index.js
+++ index.js
@@ -22,30 +22,37 @@ fetch(url)
 }
 
 window.addEventListener('hashchange', () => {
-	if (window.location.hash) {
-		let galleryid = window.location.hash.slice(1);
-		console.log(galleryid);
-		var url2 = `https://api.harvardartmuseums.org/object?apikey=${API_KEY}&gallery=${galleryid}&page=1`;
-		//use galleryid, not gallerynumber; they're not the same; learned that the hard way
-		fetch(url2)
-			.then(response2 => response2.json())
-			.then (data2 => {
-				console.log(data2);
-				data2.records.forEach(exhibit => {
-					var row = document.querySelector('#object-table').insertRow();
-					var cell1 = row.insertCell(0);
-					var cell2 = row.insertCell(1);
-					var cell3 = row.insertCell(2);
-					var cell4 = row.insertCell(3);
-					cell1.innerHTML = `${exhibit.title}`;
-					/*cell2.innerHTML = `${exhibit.groupid}`;
-					cell3.innerHTML = `${data.name}`;
-					cell4.innerHTML = "success";*/
-				});
-			});
-		showObjectsTable();
-	}
+	showObjects();
 });
+
+function showObjects() {
+	if (window.location.hash) {
+			let galleryid = window.location.hash.slice(1);
+			console.log(galleryid);
+			var url2 = `https://api.harvardartmuseums.org/object?apikey=${API_KEY}&gallery=${galleryid}&page=1`;
+			//use galleryid, not gallerynumber; they're not the same; learned that the hard way
+			fetch(url2)
+				.then(response2 => response2.json())
+				.then (data2 => {
+					console.log(data2);
+					data2.records.forEach(exhibit => {
+						var row = document.querySelector('#object-table').insertRow();
+						var cell1 = row.insertCell(0);
+						var cell2 = row.insertCell(1);
+						var cell3 = row.insertCell(2);
+						var cell4 = row.insertCell(3);
+						cell1.innerHTML = `${exhibit.title}`;
+						/*cell2.innerHTML = `${exhibit.groupid}`;
+						cell3.innerHTML = `${data.name}`;
+						cell4.innerHTML = "success";*/
+					});
+					if(data.info.next) {
+						showGalleries(data.info.next);
+					}	
+				});
+			showObjectsTable();
+	}
+}
 
 function showObjectsTable() {
 	document.querySelector("#all-objects").style.display = "block";
